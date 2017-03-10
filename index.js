fa1d4f80ceba90e1d3511add8e7e2841a4b7407a
Zoltan Nagy <n.zolika82@gmail.com>
2017-03-10 16:18:01 +0100

    add date and location

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-var postData = {"AdultCount":1,"ChildAges":[],"TicketClass":"ECO","Segments":[{"Origin":"BUD","Destination":"ROM","Depart":"2017-04-23T22:00:00.000Z","Departure":"2017-04-23"},{"Origin":"ROM","Destination":"BUD","Depart":"2017-05-09T22:00:00.000Z","Departure":"2017-05-09"}],"Culture":"en-XX","Mix":"Segments","Market":"HU","DirectOnly":false,"IncludeNearby":false};
-
-postData.Segments[0].Origin = "BUD";
-postData.Segments[0].Destination = "SYD";
-postData.Segments[1].Origin = "SYD";
-postData.Segments[1].Destination = "BUD";
-
-var settings = {  
-  "url": "http://www.momondo.com/api/3.0/FlightSearch",
-  "method": "POST",  
-  "data": JSON.stringify(postData),
-  "contentType": 'application/json; charset=utf-8',
-  "dataType": 'json',
-}
-
 minSearch = function(offers) {
     var prices = [];
     for (i = 0; i < offers.length; i++) { 
@@ -21,13 +6,13 @@ minSearch = function(offers) {
 
 var minPrice = Math.min.apply(Math, prices);
 var result = $.grep(offers, function(e){ return e.TotalPriceEUR == minPrice; });
-console.log("A legkisebb euroban számolva keresésé alapján : " + result[0]);
+//console.log("A legkisebb euroban számolva keresésé alapján : " + result[0]);
 displayResult(result[0]);
 
 }
 
 displayResult = function(result) {
-    $('<a>', {href:result.Deeplink, text:' click me to get the ticket ' + result.TotalPrice}).appendTo('body');
+    $('#myTable').append('<tr><td><a href="' + result.Deeplink + '">click me to get the ticket</td><td>Price : ' + result.TotalPrice +' HUF</td></tr>');    
 }
 
 getData = function(searchId) {
@@ -38,7 +23,7 @@ getData = function(searchId) {
             success: function(data){
             console.log("Min Price : " + data.Summary.MinPrice);
             var minResult = $.grep(data.Offers, function(e){ return e.TotalPriceEUR == data.Summary.MinPrice; });
-            console.log("A legkisebb a summary min price alapján : " + minResult[0].TotalPriceEUR);
+            //console.log("A legkisebb a summary min price alapján : " + minResult[0].TotalPriceEUR);
             displayResult(minResult[0]);
             minSearch(data.Offers);
             }
@@ -47,11 +32,29 @@ getData = function(searchId) {
 }
 
 doThis = function () {
+    //2017-05-09 document.getElementsByName('fromDate')[0].value
+    var postData = {"AdultCount":1,"ChildAges":[],"TicketClass":"ECO","Segments":[{"Origin":"","Destination":"","Depart":"","Departure":""},{"Origin":"","Destination":"","Depart":"","Departure":""}],"Culture":"en-XX","Mix":"Segments","Market":"HU","DirectOnly":false,"IncludeNearby":false};
+
+    postData.Segments[0].Origin = document.getElementsByName('from')[0].value || "BUD";
+    postData.Segments[0].Destination = document.getElementsByName('to')[0].value || "SYD";
+    postData.Segments[1].Origin = document.getElementsByName('to')[0].value || "SYD";
+    postData.Segments[1].Destination = document.getElementsByName('from')[0].value || "BUD";
+    postData.Segments[0].Departure = document.getElementsByName('fromDate')[0].value || "2017-04-24";
+    postData.Segments[1].Departure = document.getElementsByName('toDate')[0].value || "2017-05-11";
+
+    var settings = {  
+        "url": "http://www.momondo.com/api/3.0/FlightSearch",
+        "method": "POST",  
+        "data": JSON.stringify(postData),
+        "contentType": 'application/json; charset=utf-8',
+        "dataType": 'json',
+    }
+
     $.ajax(settings).done(function (response) {
-  console.log(response);
-  getData(response.SearchId);
- //console.log(settings);
-});
+    console.log(response);
+    getData(response.SearchId);
+    //console.log(settings);
+    });
 }
 
 
